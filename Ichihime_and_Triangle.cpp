70a793ed4f048aab07d4b6239d2056d5a96c3caf
lazydev01 <98304349+lazydev01@users.noreply.github.com>
2022-08-13 11:05:42 +0530

    Add files via upload

---
+++ Ichihime_and_Triangle.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    #define int long long int
+    int n;
+    cin >> n;
+    while(n--){
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int x = a;
+        int y = c;
+        int z = c;
+        cout << x << " " << y << " " << z << endl;
+    }
+    return 0;
+}
